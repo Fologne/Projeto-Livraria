afaba646616e8e1e39c2ca8a722714c5201864cb
fologne <gabrielfranfoloni01@gmail.com>
2025-09-13 21:28:14 -0400

    Comeco do projeto e criacao classe Livro

---
+++ LIVRARIA/src/br/com/livraria/livros/Livro.java
@@ -0,0 +1,21 @@
+package br.com.livraria.livros;
+public abstract class Livro{
+    private String titulo;
+    private String autor;
+    private double preco;
+    //constructor
+    public Livro(){}
+    public Livro(String titulo, String autor, double preco){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+    //setters
+    public void setTitulo(String titulo){this.titulo = titulo;}
+    public void setAutor(String autor){this.autor = autor;}
+    public void setPreco(double preco){this.preco = preco;}
+    //getters
+    public String getTitulo(){return titulo;}
+    public String getAutor(){return autor;}
+    public double getPreco(){return preco;}
+}
